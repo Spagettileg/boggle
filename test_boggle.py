7ee6226323209a01fe2614da1f545a7b483f961d
Ubuntu <ubuntu@ip-172-31-30-91.eu-west-1.compute.internal>
2019-06-11 23:30:10 +0000

    Initial commit

--- test_boggle.py
+++ test_boggle.py
@@ -80,6 +80,47 @@ class TestBoggle(unittest.TestCase):
             others.remove(pos)
             self.assertListEqual(sorted(neighbours[pos]), sorted(others)) # Asserts the positions of neighbours being checked. 
             
+    def test_converting_a_path_to_a_word(self):
+        """
+        Ensure that paths can be converted to words
+        """
+        
+        grid = boggle.make_grid(2, 2)
+        oneLetterWord = boggle.path_to_word(grid, [(0, 0)]) # To turn lists of positions into strings of letters.
+        twoLetterWord = boggle.path_to_word(grid, [(0, 0), (1, 1)])
+        self.assertEqual(oneLetterWord, grid[(0, 0)])
+        self.assertEqual(twoLetterWord, grid[(0, 0)] + grid[(1, 1)])
+        
+    def test_search_grid_for_words(self):
+        """
+        Ensure that certain patterns can be found in a path_to_word
+        """
+        
+        grid = {(0, 0): 'A', (0, 1): 'B', (1, 0): 'C', (1, 1): 'D'}
+        twoLetterWord = 'AB'
+        threeLetterWord = 'ABC'
+        notThereWord = 'EEE'
+        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+        
+        foundWords = boggle.search(grid, dictionary)
+        
+        self.assertTrue(twoLetterWord in foundWords)
+        self.assertTrue(threeLetterWord in foundWords)
+        self.assertTrue(notThereWord not in foundWords)
+        
+    def test_load_dictionary(self):
+        """
+        Test that the 'get_dictionary' function returns a dictionary
+        that has a length greater than 0 (zero)
+        """
+        
+        dictionary = boggle.get_dictionary('words.txt')
+        self.assertGreater(len(dictionary), 0)
+        
+        
+        
+        
+            
             
          
         
