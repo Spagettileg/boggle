2c48e5d72308ecbc3e760c2148d6e254f5137798
Ubuntu <ubuntu@ip-172-31-30-91.eu-west-1.compute.internal>
2019-06-11 10:21:51 +0000

    Initial commit

---
+++ test_boggle.py
@@ -0,0 +1,7 @@
+import unittest
+
+class test_boggle(unittest.TestCase): # The 'unittest' framework offers a dropdown selection containing various Classes
+    def test_is_this_thing_on(self): # Method
+        self.assertEqual(1,0) # Trivial test to find out if 1 = 0
+        
+    
