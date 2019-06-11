8886fc37e22e219cd3efbbec1609447a42dda9cc
Ubuntu <ubuntu@ip-172-31-30-91.eu-west-1.compute.internal>
2019-06-11 23:29:41 +0000

    Initial commit

--- boggle.py
+++ boggle.py
@@ -52,6 +52,52 @@ def all_grid_neighbours(grid):
         
     return neighbours
     
+def path_to_word(grid, path):
+    """
+    Add all of the letters on the path to a string
+    """
+    
+    return ''.join([grid[p] for p in path])
+    
+def search(grid, dictionary):
+    """
+    Search through the paths to locate words by matching
+    strings to words in a dictionary
+    """
+    
+    neighbours = all_grid_neighbours(grid) # Get the neighbours of every position
+    paths = [] # Get paths list to capture all paths
+    
+    def do_search(path):
+        word = path_to_word(grid, path)
+        if word in dictionary:
+            paths.append(path)
+        for next_pos in neighbours[path[-1]]:
+            if next_pos not in path:
+                do_search(path + [next_pos])
+                
+    for position in grid:
+        do_search([position])
+        
+    words = []
+    for path in paths:
+        words.append(path_to_word(grid, path))
+    return set(words)
+    
+def get_dictionary(dictionary_file):
+    """
+    Dictionary code that loads the dictionary_file
+    """
+    
+    with open(dictionary_file) as f:
+        return [w.strip().upper() for w in f]
+        
+    
+    
+    
+    
+    
+    
         
         
     
