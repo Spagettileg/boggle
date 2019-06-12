097a21ec2bdda412d80a9c64bf304a08c08f725d
Ubuntu <ubuntu@ip-172-31-30-91.eu-west-1.compute.internal>
2019-06-12 19:24:25 +0000

    Initial commit

--- test_boggle.py
+++ test_boggle.py
@@ -100,7 +100,9 @@ class TestBoggle(unittest.TestCase):
         twoLetterWord = 'AB'
         threeLetterWord = 'ABC'
         notThereWord = 'EEE'
-        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+        fullwords = [twoLetterWord, threeLetterWord, notThereWord]
+        stems = ['A', 'AB', 'E', 'EE']
+        dictionary = fullwords, stems
         
         foundWords = boggle.search(grid, dictionary)
         
