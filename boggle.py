60b24ab060d5d1de63f7e68a6c5badcff9e0a892
Ubuntu <ubuntu@ip-172-31-30-91.eu-west-1.compute.internal>
2019-06-12 00:00:34 +0000

    Initial commit

--- boggle.py
+++ boggle.py
@@ -92,7 +92,26 @@ def get_dictionary(dictionary_file):
     with open(dictionary_file) as f:
         return [w.strip().upper() for w in f]
         
+def main():
+    """
+    This is the function that will run the whole project
+    The code below represents a high level of abstraction
+    """
     
+    grid = make_grid(3, 3)
+    dictionary = get_dictionary('words.txt')
+    words = search(grid, dictionary)
+    for word in words:
+        print(word)
+    print("Found %s words" % len(words))
+    
+if __name__ == "__main__":
+    main()
+    
+    
+
+
+        
     
     
     
